557df117690bea9219ea0fa759482658ab13ffd4
Wesley <Wesley@DESKTOP-BH6M3VH>
2023-08-01 00:15:54 -0300

    vendo se esse trein ta dando certo mesmo

--- funfanfoJava/src/funfanfoJava/Funfada.java
+++ funfanfoJava/src/funfanfoJava/Funfada.java
@@ -4,7 +4,7 @@ public class Funfada {
 
 	public static void main(String[] args) {
 		System.out.println("Entao toma essa fundada java bosta");
-
+		System.out.println("bora ver se esse trein funcionou mesmo");
 	}
 
 }

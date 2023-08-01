2fb8f93eb83c48813c688ebbbecbc89c1ea51cdb
Wesley <Wesley@DESKTOP-BH6M3VH>
2023-08-01 00:26:52 -0300

    commit estou começando tudo de novo

--- funfanfoJava/src/funfanfoJava/Funfada.java
+++ funfanfoJava/src/funfanfoJava/Funfada.java
@@ -3,9 +3,7 @@ package funfanfoJava;
 public class Funfada {
 
 	public static void main(String[] args) {
-		System.out.println("Entao toma essa fundada java bosta");
-		System.out.println("bora ver se esse trein funcionou mesmo");
-		System.out.println("parece que deu certo");
+		System.out.println("Comecando tudo de novo");
 	}
 
 }
